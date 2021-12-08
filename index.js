2ec0aca63098a597ce3a4d5b352f89e037fefb42
gabilucarini <gabilucarini18@gmail.com>
2021-12-08 16:28:45 -0300

    final_v1

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 let add = document.getElementById("add-city");
-let ciudad = document.getElementById("eleccion-ciudad");
-var lista_ciudad = []
+let ciudad = document.getElementById("ciudad");
+let lista_ciudad = []
+let temp = document.getElementById('temp');
+let nombre = document.getElementById('name');
+let desc = document.getElementById('desc');
 
-function addcity() {
+
+
+function AgregarCiudad() {
     if (lista_ciudad.includes(add.value) == false) {
-        lista_ciudad.push(add.value);
-        return localStorage.setItem("Ciudad",JSON.stringify(lista_ciudad));
+        if(add.value != "")
+            lista_ciudad.push(add.value);
+            return localStorage.setItem("Ciudad",JSON.stringify(lista_ciudad));
     }
     
 }
 
-function CargaCiudades() {
-    let text = localStorage.getItem("Ciudad");
-    let asd = JSON.parse(text);
+function CargaCiudades(){
+    const array_ciudades = JSON.parse(localStorage.getItem("Ciudad"));
+    array_ciudades.forEach(array => {        
+        let opt = document.createElement('option');
+        opt.value = array; 
+        opt.text = array;
+        ciudad.appendChild(opt);
+    });
 }
 
-let aaa = localStorage.getItem("Ciudad");
-let asd = JSON.parse(aaa);
 
-for (let index = 0; index < asd.length; index++) {
-    ciudad.options
+
+function ApiClima(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ciudad.value+'&appid=3936d0749fdc3124c6566ed26cf11978')
+    .then(response => response.json())
+    .then(data => {
+      var tempValue = data['main']['temp'];
+      var nameValue = data['name'];
+      var descValue = data['weather'][0]['description'];
+        
+      tempValue = tempValue - 273.15
+      nombre.innerHTML = nameValue;
+      desc.innerHTML = "Descripcion - "+descValue;
+      temp.innerHTML = "Temperatura - "+tempValue.toFixed(2);
     
-}
+    })
+    
+    .catch(err => alert("Wrong city name!"));
+    }
